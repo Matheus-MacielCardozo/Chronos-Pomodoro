0a902ce26e61bf8ca5d538cdfe8e54f35de2d256
Matheus Maciel <matheus.macielcardozo@gmail.com>
2026-01-15 16:56:19 -0300

    Update: Prettier Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 export function App() {
-    
-
-    return (
-        <>
-            <h1>Olá Mundo (do app)</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla neque placeat quod aliquid facilis quo iste impedit ducimus laboriosam laudantium beatae eligendi, fugiat aut distinctio? Accusantium illum animi debitis autem.
-            </p>
-        </>
-    )
+  return (
+    <>
+      <h1>Olá Mundo (do app)</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla neque
+        placeat quod aliquid facilis quo iste impedit ducimus laboriosam
+        laudantium beatae eligendi, fugiat aut distinctio? Accusantium illum
+        animi debitis autem.
+      </p>
+    </>
+  );
 }
