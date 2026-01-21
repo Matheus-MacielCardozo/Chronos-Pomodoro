a05e8ed93171697a59ce395c5438e5cf3810bf54
Matheus Maciel <matheus.macielcardozo@gmail.com>
2026-01-21 12:06:31 -0300

    Feature: First Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
+
+import { Container } from './components/Container'
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo!
-        <button>
-          <TimerIcon />
-        </button>
-
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla neque
-        placeat quod aliquid facilis quo iste impedit ducimus laboriosam
-        laudantium beatae eligendi, fugiat aut distinctio? Accusantium illum
-        animi debitis autem.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
     </>
   );
 }
