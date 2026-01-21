3cd0869b0f5549742443d74cd34f37029646a6dd
Matheus Maciel <matheus.macielcardozo@gmail.com>
2026-01-21 14:10:27 -0300

    Feature: Cycles and DefaultButton Components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function App() {
     return (
@@ -38,12 +41,11 @@ export function App() {
                     </div>
 
                     <div className='formRow'>
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0</p>
+                        <Cycles />
                     </div>
 
                     <div className='formRow'>
-                        <button>enviar</button>
+                        <DefaultButton icon={<PlayCircleIcon />} />
                     </div>
                 </form>
             </Container>
